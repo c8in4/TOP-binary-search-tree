232a6b7d4b139210371d05994659bd4493a9781a
c8in4 <106409156+c8in4@users.noreply.github.com>
2025-09-10 07:38:01 +1200

    Create Node class

---
+++ script.js
@@ -0,0 +1,15 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+    constructor(arr) {
+        this.root = buildTree(arr)
+    }
+
+    
+}
